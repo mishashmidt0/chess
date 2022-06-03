094db6a18817e28ac579912f7375ae72ee7b0c87
Misha Shmidt <mishashmidt0@gmail.com>
2022-06-03 23:59:18 +0300

    fix: king step

--- src/modules/figures/King.ts
+++ src/modules/figures/King.ts
@@ -5,7 +5,7 @@ import {Cell} from "../Cell";
 import blackLogo from "../../assets/black-king.png"
 import whiteLogo from "../../assets/white-king.png"
 
-export class King extends Figure{
+export class King extends Figure {
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
 
@@ -16,7 +16,11 @@ export class King extends Figure{
     canMove(target: Cell): boolean {
         if (!super.canMove(target)) return false
 
+        const dx = Math.abs(this.cell.x - target.x);
+        const dy = Math.abs(this.cell.y - target.y);
 
+        return (dx === 1 && dy === 2) || (dx === 2 && dy === 1)
+        
         return true
     }
 }
